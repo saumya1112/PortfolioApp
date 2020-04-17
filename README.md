33829a999d3cb8f91c357a7ccccd2371c4d543ab
Abhishek Chaudhary <abhishek.chaudhary.in@gmail.com>
2020-04-17 21:56:18 +0530

    Completed Part 1

---
+++ README.md
@@ -0,0 +1,4 @@
+# Portfolio App
+
+### Part 1
+<img src="assets/app_portfolio_part_1.png" width="40%"/>
--- app/src/main/java/io/github/abhishek/portfolioapp/MainActivity.java
+++ app/src/main/java/io/github/abhishek/portfolioapp/MainActivity.java
@@ -1,6 +1,8 @@
 package io.github.abhishek.portfolioapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * A click action for general view. Attached to GitHub button in XML.
+     */
+    public void openGithub(View view) {
+        Toast.makeText(this, "Open GitHub", Toast.LENGTH_SHORT).show();
     }
 }
